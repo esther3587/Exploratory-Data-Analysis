4c02f056e8aba59c4f9499cafc5f2eb983e729eb
Shun-Wen Chang <esther3587@users.noreply.github.com>
2016-11-03 11:16:21 -0700

    Create plot_2.R

---
+++ plot_2.R
@@ -0,0 +1,7 @@
+power <- read.table("household_power_consumption.txt",header = TRUE,sep=';',skip = 66636, nrows = 2880)
+colnames(power) <- c('Date','Time','Global_active_power','Global_reactive_power','Voltage','Global_intensity','Sub_metering_1','Sub_metering_2','Sub_metering_3')
+power$datetime <- paste(power$Date,power$Time)
+power$datetime <- strptime(power$datetime,format="%d/%m/%Y %H:%M:%S")
+png(filename = "plot2.png",width = 480, height = 480, unit = "px")
+with(power,plot(datetime,Global_active_power,type='l',xlab = '', ylab = 'Global Active Power (kilowatts)'))
+dev.off()
